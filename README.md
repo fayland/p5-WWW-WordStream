18949d85b5f357874d87bf7a430371241a70ffaf
Fayland Lam <fayland@gmail.com>
2013-12-06 14:50:22 +0800

    with milla new

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+WWW::WordStream - Blah blah blah
+
+# SYNOPSIS
+
+    use WWW::WordStream;
+
+# DESCRIPTION
+
+WWW::WordStream is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2013- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/WWW/WordStream.pm
@@ -0,0 +1,39 @@
+package WWW::WordStream;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WWW::WordStream - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use WWW::WordStream;
+
+=head1 DESCRIPTION
+
+WWW::WordStream is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
